8bab0eaadb6c9003ed198db2bb08e020f6d75051
Cesar Tolaba <cesartolaba3@gmail.com>
2021-07-17 17:15:14 -0300

    creacion del formulario

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ let port = 3000;
 
 app.use(express.static('public'));
 
+
+// index.html
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, './views/home.html')) });
+
+
+// Register.html
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'))
+})
+
+/* login.html */
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'))
+})
+
 
 app.listen(port, () => console.log(`Servidor corriendo en el ${port}\n http://localhost:${port}`));
